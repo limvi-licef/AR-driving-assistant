e030b7a7abcf1855d32b76473383c5f0cc3fd03b
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-09-16 16:50:56 -0400

    Added button in settings to move the canvas

---
+++ Assets/Resources/Scripts/AlwaysLookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlwaysLookAtCamera : MonoBehaviour {
+
+    public Camera cam;
+    bool movingUI = false;
+
+	void Update () {
+        transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position);
+        transform.position = movingUI ? cam.transform.position + cam.transform.forward * 2 : transform.position;
+    }
+
+    public void MoveUI()
+    {
+        movingUI = !movingUI;
+    }
+}
---
+++ Assets/Resources/Scripts/FollowMainCamera.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMainCamera : MonoBehaviour {
+
+	void Update () {
+        transform.position = Camera.main.transform.position;
+	}
+}
