ae377e79776b1159570ee3caa82799e28f739896
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-09-22 16:30:34 -0400

    Changed ui hologram to follow gaze on axis x

--- Assets/HoloToolkit/SpatialMapping/Scripts/TapToPlaceUI.cs
+++ Assets/HoloToolkit/SpatialMapping/Scripts/TapToPlaceUI.cs
@@ -4,17 +4,21 @@ namespace HoloToolkit.Unity
 {
     public partial class TapToPlaceUI : MonoBehaviour
     {
-        public float distanceToCamera = 2f;
+        const float scaleDifference = 500;
+        public GameObject UIComponent;
+        public float DistanceToCamera = 2f;
         bool placing = false;
         Vector3 colliderSize;
         Vector3 placingColliderSize;
-        Vector3 localPosition;
+        Vector3 newPosition = Vector3.zero;
+        Vector3 targetPosition;
+        float fixedAngle;
 
         void Start()
         {
             colliderSize = gameObject.GetComponent<BoxCollider>().size;
             placingColliderSize = new Vector3(colliderSize.x * 10, colliderSize.y * 10, colliderSize.z);
-            localPosition = (transform.position - Camera.main.transform.position).normalized * distanceToCamera;
+            fixedAngle = Camera.main.transform.forward.y;
         }
 
         void OnSelect()
@@ -26,15 +30,32 @@ namespace HoloToolkit.Unity
         {
             if(placing)
             {
-                transform.position = Camera.main.transform.position + Camera.main.transform.forward * distanceToCamera;
-                localPosition = (transform.position - Camera.main.transform.position).normalized * distanceToCamera;
+                transform.position = (Camera.main.transform.position + Camera.main.transform.forward * DistanceToCamera);
+                fixedAngle = Camera.main.transform.forward.y;
                 gameObject.GetComponent<BoxCollider>().size = placingColliderSize;
             }
             else
             {
                 gameObject.GetComponent<BoxCollider>().size = colliderSize;
-                transform.position = localPosition + Camera.main.transform.position;
+                targetPosition = new Vector3(Camera.main.transform.forward.x, fixedAngle, Camera.main.transform.forward.z) * DistanceToCamera + Camera.main.transform.position;
+                //transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime);
+                float distance = Vector3.Distance(UIComponent.GetComponent<RectTransform>().position, targetPosition) * scaleDifference;
+                float width = UIComponent.GetComponent<RectTransform>().rect.width;
+
+                if (distance > width * 1.5)
+                {
+                    transform.position = Vector3.Slerp(transform.position, targetPosition, Time.deltaTime);
+                    //Vector3 lerpTargetPosition = targetPosition;
+                    //lerpTargetPosition = Vector3.Lerp(transform.position, lerpTargetPosition, 1f);
+                    //newPosition = Interpolator.NonLinearInterpolateTo(transform.position, lerpTargetPosition, Time.deltaTime, 9.8f);
+                    //if ((targetPosition - newPosition).sqrMagnitude <= 0.0000001f)
+                    //{
+                    //    newPosition = targetPosition;
+                    //}
+                    //transform.position = newPosition;
+                }
             }
         }
+
     }
 }
