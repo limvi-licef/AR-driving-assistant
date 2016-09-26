0cacd9ae530be11742fb2f8821260d7427d43e11
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-09-26 11:39:26 -0400

    Initial Android project setup

---
+++ AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.limvi_licef.ar_driving_assistant;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
