1b0efe2749009914edec8cd5466404370d1d0a80
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-10-07 15:19:44 -0400

    Unregister listeners before destroy

--- AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/Activities/MainActivity.java
+++ AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/Activities/MainActivity.java
@@ -51,8 +51,8 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
         unregisterListeners();
+        super.onDestroy();
     }
 
     private void setupUIElements() {
