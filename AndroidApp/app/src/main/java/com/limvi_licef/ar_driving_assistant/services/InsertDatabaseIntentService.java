bd65ab5c6bf918c5b81ecc1e41070623a530fee2
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-10-07 11:55:14 -0400

    Override onStartCommand
    Add Debug Logs

--- AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/services/InsertDatabaseIntentService.java
+++ AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/services/InsertDatabaseIntentService.java
@@ -4,6 +4,7 @@ import android.app.IntentService;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.limvi_licef.ar_driving_assistant.database.DatabaseHelper;
 
@@ -24,11 +25,21 @@ public class InsertDatabaseIntentService extends IntentService {
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, startId, startId);
+        Log.d("Insert Service", "Started Insert Service");
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d("Insert Service", "Started Intent Handling");
         String tableName = (String) intent.getExtras().get(TABLE_NAME);
         ContentValues values = (ContentValues) intent.getExtras().get(VALUES);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         db.insert(tableName, null, values);
+        Log.d("Insert Service Values", values.toString());
     }
 
 }
