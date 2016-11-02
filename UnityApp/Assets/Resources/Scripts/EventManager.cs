85d45aa93f620096917d3f4ab6bb0594b7459199
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-11-02 11:06:13 -0400

    Change events list to static dictionary

--- UnityApp/Assets/Resources/Scripts/EventManager.cs
+++ UnityApp/Assets/Resources/Scripts/EventManager.cs
@@ -10,19 +10,21 @@ public class EventManager : MonoBehaviour
     public delegate void TabClickAction();
     public static event TabClickAction OnTabClick;
 
-    private List<Event> m_Events;
+    private static Dictionary<string, Event> m_Events;
 
     void Start()
     {
-        m_Events = new List<Event>();
-        m_Events.Add(new Event() { Prefab = "AdviceDisplay", Sound = "WarningBeep", Color = new Color32(16,184,31,255), Icon = "info_icon", Text = "" });
-        m_Events.Add(new Event() { Prefab = "AdviceDisplay", Sound = "WarningBeep", Color = Color.blue, Icon = "advice_icon", Text = "" });
-        m_Events.Add(new Event() { Prefab = "LeftWarningDisplay", Sound = "WarningBeep", Color = Color.red, Icon = "warning_icon", Text = "" });
-        m_Events.Add(new Event() { Prefab = "RightWarningDisplay", Sound = "WarningBeep", Color = Color.red, Icon = "warning_icon", Text = "" });
+        m_Events = new Dictionary<string, Event>();
+        m_Events["Information"] = new Event() { Prefab = "AdviceDisplay", Sound = "WarningBeep", Color = new Color32(16, 184, 31, 255), Icon = "info_icon", Text = "" };
+        m_Events["Advice"] = new Event() { Prefab = "AdviceDisplay", Sound = "WarningBeep", Color = Color.blue, Icon = "advice_icon", Text = "" };
+        m_Events["LeftWarning"] = new Event() { Prefab = "LeftWarningDisplay", Sound = "WarningBeep", Color = Color.red, Icon = "warning_icon", Text = "" };
+        m_Events["RightWarning"] = new Event() { Prefab = "RightWarningDisplay", Sound = "WarningBeep", Color = Color.red, Icon = "warning_icon", Text = "" };
     }
 
-    public static void SendEvent(Event e)
+    public static void SendEvent(string name, string message)
     {
+        Event e = m_Events[name];
+        e.Text = message;
         if (OnTrigger != null)
         {
             OnTabClick();
