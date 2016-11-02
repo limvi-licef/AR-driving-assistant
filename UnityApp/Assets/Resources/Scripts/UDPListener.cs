d9d1f1dbe3ddf8ecc1da38b8e46f1e01f8a91be7
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-11-02 12:59:06 -0400

    Add UDP listener to allow Android app to send events to Unity app

---
+++ UnityApp/Assets/Resources/Scripts/UDPListener.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+#if !UNITY_EDITOR
+    using Windows.Networking.Sockets;
+#endif
+
+/*
+ * Adapted from https://forums.hololens.com/discussion/comment/9837
+ */
+
+public class UDPListener : MonoBehaviour
+{
+#if !UNITY_EDITOR
+    private DatagramSocket socket;
+#endif
+
+    private const string PORT = "12345";
+
+    void Start()
+    {
+        Debug.Log(Application.platform);
+#if !UNITY_EDITOR
+        Server();
+#endif
+    }
+
+    void Update()
+    {
+
+    }
+
+#if !UNITY_EDITOR
+    private async void Server()
+    {
+        socket = new DatagramSocket();
+        socket.MessageReceived += Socket_MessageReceived;
+
+        try
+        {
+            await socket.BindEndpointAsync(null, "12345");
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+            Debug.Log(SocketError.GetStatus(e.HResult).ToString());
+            return;
+        }
+    }
+#endif
+
+#if !UNITY_EDITOR
+    private async void Socket_MessageReceived(Windows.Networking.Sockets.DatagramSocket sender,
+        Windows.Networking.Sockets.DatagramSocketMessageReceivedEventArgs args)
+    {
+        //Read the message that was received from the UDP echo client.
+        Stream streamIn = args.GetDataStream().AsStreamForRead();
+        StreamReader reader = new StreamReader(streamIn);
+        string message = await reader.ReadLineAsync();
+
+        string[] tokens = message.Split(';');
+        UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+        {
+            EventManager.SendEvent(tokens[0], tokens[1]);
+        }, false);
+    }
+#endif
+}
