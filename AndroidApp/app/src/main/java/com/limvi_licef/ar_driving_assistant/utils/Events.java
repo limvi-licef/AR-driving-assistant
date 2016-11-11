a73ffd28eb21949e8011ee33fd82f21be15943cd
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-11-11 15:15:51 -0500

    Add getAllevents utils method

--- AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/utils/Events.java
+++ AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/utils/Events.java
@@ -1,11 +1,14 @@
 package com.limvi_licef.ar_driving_assistant.utils;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.util.Log;
 
 import com.fastdtw.timeseries.TimeSeries;
 import com.fastdtw.timeseries.TimeSeriesBase;
 import com.limvi_licef.ar_driving_assistant.R;
+import com.limvi_licef.ar_driving_assistant.database.DatabaseContract;
+import com.limvi_licef.ar_driving_assistant.database.DatabaseHelper;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -39,6 +42,26 @@ public final class Events {
         }
         return context.getResources().getString(R.string.send_event_task_success);
     }
+
+    public static List<Event> getAllEvents(Context context){
+        List<Event> events = new ArrayList<>();
+        Cursor eventCursor = DatabaseHelper.getHelper(context).getWritableDatabase().query(DatabaseContract.TrainingEvents.TABLE_NAME,
+                new String[]{DatabaseContract.TrainingEvents.START_TIMESTAMP, DatabaseContract.TrainingEvents.END_TIMESTAMP, DatabaseContract.TrainingEvents.LABEL},
+                DatabaseContract.TrainingEvents.CURRENT_USER_ID + " = ?",
+                new String[]{Preferences.getCurrentUserId(context)}, null, null, null);
+        int startTimestampColumnIndex = eventCursor.getColumnIndexOrThrow(DatabaseContract.TrainingEvents.START_TIMESTAMP);
+        int endTimestampColumnIndex = eventCursor.getColumnIndexOrThrow(DatabaseContract.TrainingEvents.END_TIMESTAMP);
+        int labelColumnIndex = eventCursor.getColumnIndexOrThrow(DatabaseContract.TrainingEvents.LABEL);
+        int typeColumnIndex = eventCursor.getColumnIndexOrThrow(DatabaseContract.TrainingEvents.TYPE);
+        int messageColumnIndex = eventCursor.getColumnIndexOrThrow(DatabaseContract.TrainingEvents.MESSAGE);
+
+        while (eventCursor.moveToNext()) {
+            events.add(new Event(eventCursor.getString(labelColumnIndex), eventCursor.getLong(startTimestampColumnIndex), eventCursor.getLong(endTimestampColumnIndex),
+                    EventTypes.valueOf(eventCursor.getString(typeColumnIndex)),eventCursor.getString(messageColumnIndex)));
+        }
+
+        eventCursor.close();
+        return events;
     }
 
     public static TimeSeries createTimeSeriesFromSensor(Context context, long startTimestamp, long endTimestamp, String tableName, String... valueColumnNames) {
