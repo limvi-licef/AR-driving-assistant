61b54f24dc505e5e84fb4684031a9cb3a92b20f6
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-11-15 15:52:40 -0500

    Move DTW algorithm to its own class Add interface to process all events with all algorithms

---
+++ AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/algorithms/EventAlgorithm.java
@@ -0,0 +1,7 @@
+package com.limvi_licef.ar_driving_assistant.algorithms;
+
+import com.limvi_licef.ar_driving_assistant.utils.Events.Event;
+
+public interface EventAlgorithm {
+    void processEvent(Event event);
+}
--- AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/tasks/MatchEventTask.java
+++ AndroidApp/app/src/main/java/com/limvi_licef/ar_driving_assistant/tasks/MatchEventTask.java
@@ -1,20 +1,14 @@
 package com.limvi_licef.ar_driving_assistant.tasks;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
-import android.util.Log;
 
-import com.fastdtw.dtw.FastDTW;
-import com.fastdtw.timeseries.TimeSeries;
-import com.fastdtw.util.Distances;
-import com.limvi_licef.ar_driving_assistant.R;
-import com.limvi_licef.ar_driving_assistant.database.DatabaseContract;
-import com.limvi_licef.ar_driving_assistant.database.DatabaseHelper;
-import com.limvi_licef.ar_driving_assistant.utils.Broadcasts;
-import com.limvi_licef.ar_driving_assistant.utils.Config;
+import com.limvi_licef.ar_driving_assistant.algorithms.DynamicTimeWarpingAlgorithm;
+import com.limvi_licef.ar_driving_assistant.algorithms.EventAlgorithm;
 import com.limvi_licef.ar_driving_assistant.utils.Events;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MatchEventTask extends AsyncTask<Void, Void, Void> {
 
@@ -36,71 +30,15 @@ public class MatchEventTask extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
 
-        Double closestAcceleration = null;
-        Double closestRotation = null;
-        Double closestSpeed = null;
+        List<EventAlgorithm> algorithms = new ArrayList<>();
+        algorithms.add(new DynamicTimeWarpingAlgorithm(context, startTimestamp, endTimestamp));
 
         for (Events.Event event : Events.getAllEvents(context)) {
-
-            for(long start = startTimestamp, stop = startTimestamp + event.duration; stop < this.endTimestamp;
-                            start += Config.DynamicTimeWarping.TIME_BETWEEN_SEGMENTS, stop += Config.DynamicTimeWarping.TIME_BETWEEN_SEGMENTS){
-
-                TimeSeries eventAccel, eventRotation, eventSpeed, segmentAccel, segmentRotation, segmentSpeed;
-                try {
-                    eventAccel = Events.createTimeSeriesFromSensor(context, event.startTimestamp, event.endTimestamp, DatabaseContract.LinearAccelerometerData.TABLE_NAME,
-                            DatabaseContract.LinearAccelerometerData.AXIS_X, DatabaseContract.LinearAccelerometerData.AXIS_Y, DatabaseContract.LinearAccelerometerData.AXIS_Z);
-                    eventRotation = Events.createTimeSeriesFromSensor(context, event.startTimestamp, event.endTimestamp, DatabaseContract.RotationData.TABLE_NAME, DatabaseContract.RotationData.AZIMUTH);
-                    eventSpeed = Events.createTimeSeriesFromSensor(context, event.startTimestamp, event.endTimestamp, DatabaseContract.SpeedData.TABLE_NAME, DatabaseContract.SpeedData.SPEED);
-
-                    segmentAccel = Events.createTimeSeriesFromSensor(context, start, stop, DatabaseContract.LinearAccelerometerData.TABLE_NAME,
-                            DatabaseContract.LinearAccelerometerData.AXIS_X, DatabaseContract.LinearAccelerometerData.AXIS_Y, DatabaseContract.LinearAccelerometerData.AXIS_Z);
-                    segmentRotation = Events.createTimeSeriesFromSensor(context, start, stop, DatabaseContract.RotationData.TABLE_NAME, DatabaseContract.RotationData.AZIMUTH);
-                    segmentSpeed = Events.createTimeSeriesFromSensor(context, start, stop, DatabaseContract.SpeedData.TABLE_NAME, DatabaseContract.SpeedData.SPEED);
-                } catch(IndexOutOfBoundsException e) {
-                    Log.d("DTW", "No data in segment");
-                    continue;
-                }
-
-                double distanceAccel = FastDTW.compare(eventAccel, segmentAccel, Config.DynamicTimeWarping.SEARCH_RADIUS, Distances.EUCLIDEAN_DISTANCE).getDistance();
-                double distanceRotation = FastDTW.compare(eventRotation, segmentRotation, Config.DynamicTimeWarping.SEARCH_RADIUS, Distances.EUCLIDEAN_DISTANCE).getDistance();
-                double distanceSpeed = FastDTW.compare(eventSpeed, segmentSpeed, Config.DynamicTimeWarping.SEARCH_RADIUS, Distances.EUCLIDEAN_DISTANCE).getDistance();
-
-                closestAcceleration = (closestAcceleration == null || distanceAccel < closestAcceleration ) ? distanceAccel : closestAcceleration;
-                closestRotation = (closestRotation == null || distanceRotation < closestRotation ) ? distanceRotation : closestRotation;
-                closestSpeed = (closestSpeed == null || distanceSpeed < closestSpeed ) ? distanceSpeed : closestSpeed;
-
-                if(distanceAccel < Config.DynamicTimeWarping.ACCELERATION_DISTANCE_CUTOFF
-                        && distanceRotation < Config.DynamicTimeWarping.ROTATION_DISTANCE_CUTOFF
-                        && distanceSpeed < Config.DynamicTimeWarping.SPEED_DISTANCE_CUTOFF){
-                    matchFound(event);
-                }
-                saveResults(event, start, stop, distanceAccel, distanceRotation, distanceSpeed);
+            for(EventAlgorithm algorithm : algorithms) {
+                algorithm.processEvent(event);
             }
         }
-        Broadcasts.sendWriteToUIBroadcast(context, "Closest Acceleration : " + closestAcceleration + " Closest Rotation : " + closestRotation + " Closest Speed : " + closestSpeed);
-        Broadcasts.sendWriteToUIBroadcast(context, "DTW done");
         return null;
     }
 
-    private void matchFound(Events.Event e){
-        Broadcasts.sendWriteToUIBroadcast(context, context.getResources().getString(R.string.match_event_task_match_found) + e.label);
-        String status = Events.sendEvent(context, e.type.name(), e.message);
-        Broadcasts.sendWriteToUIBroadcast(context, context.getResources().getString(R.string.match_event_task_status) + status);
-    }
-
-    private void saveResults(Events.Event event, long segmentStart, long segmentStop, double distanceAccel, double distanceRotation, double distanceSpeed) {
-        SQLiteDatabase db = DatabaseHelper.getHelper(context).getWritableDatabase();
-        db.beginTransaction();
-        ContentValues values = new ContentValues();
-        values.put(DatabaseContract.ResultsDTW.EVENT_LABEL, event.label);
-        values.put(DatabaseContract.ResultsDTW.EVENT_DURATION, event.duration);
-        values.put(DatabaseContract.ResultsDTW.SEGMENT_START, segmentStart);
-        values.put(DatabaseContract.ResultsDTW.SEGMENT_STOP, segmentStop);
-        values.put(DatabaseContract.ResultsDTW.DISTANCE_ACCELERATION, distanceAccel);
-        values.put(DatabaseContract.ResultsDTW.DISTANCE_ROTATION, distanceRotation);
-        values.put(DatabaseContract.ResultsDTW.DISTANCE_SPEED, distanceSpeed);
-        db.insert(DatabaseContract.ResultsDTW.TABLE_NAME, null, values);
-        db.setTransactionSuccessful();
-        db.endTransaction();
-    }
 }
