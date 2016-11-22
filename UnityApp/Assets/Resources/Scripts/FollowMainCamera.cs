20d83a4b67a39e112345142a25cf16c54be48f44
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-11-22 12:31:20 -0500

    Remove unused script

--- UnityApp/Assets/Resources/Scripts/FollowMainCamera.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class FollowMainCamera : MonoBehaviour {
-
-	void Update () {
-        transform.position = Camera.main.transform.position;
-	}
-}
