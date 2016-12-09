075d4564e483a0a7710e8e58fccbaff7fa952d60
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-12-09 12:21:00 -0500

    Add flip screen button to android scene

---
+++ UnityApp/Assets/Resources/Scripts/ReverseScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// From http://answers.unity3d.com/questions/353680/mirror-flip-camera.html
+/// </summary>
+public class ReverseScreen : MonoBehaviour {
+
+    public Camera Camera;
+
+    /// <summary>
+    /// Flips the screen on the X axis
+    /// </summary>
+	public void toggleReverse()
+    {
+        Camera.projectionMatrix = Camera.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
+    }
+}
