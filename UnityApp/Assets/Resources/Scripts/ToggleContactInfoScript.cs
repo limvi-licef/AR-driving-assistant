abc8ada2a06db6426ccbde2143542ec7b23d4b49
Julien Brochu-Renaud <julienbrochurenaud@gmail.com>
2016-12-14 12:54:26 -0500

    [UnityApp] Fix bug where settings screen panels would be disabled instead of showing Fix button transition

--- UnityApp/Assets/Resources/Scripts/ToggleContactInfoScript.cs
+++ UnityApp/Assets/Resources/Scripts/ToggleContactInfoScript.cs
@@ -8,11 +8,6 @@ public class ToggleContactInfoScript : MonoBehaviour {
 
     bool toggleBool = false;
 
-    void Start()
-    {
-        gameObject.SetActive(false);
-    }
-
     public void ToggleContactInfo()
     {
         if(!toggleBool) gameObject.GetComponentInParent<RectTransform>().SetAsLastSibling();
